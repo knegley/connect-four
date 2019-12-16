c43df36d5ceb44604ab8dfb1bc0ff3f8895e06a6
Feeblebird <joeyrocks1999@gmail.com>
2019-12-16 14:18:27 -0500

    Basic layout in HTML/CSS and some notes in js file

---
+++ connectFour.js
@@ -0,0 +1,12 @@
+const columns = document.getElementsByClassName('column')
+// In case you need tags for individual columns. Feel free to remove these if you don't need them.
+const column1 = document.getElementById('c1')
+const column2 = document.getElementById('c2')
+const column3 = document.getElementById('c3')
+const column4 = document.getElementById('c4')
+const column5 = document.getElementById('c5')
+const column6 = document.getElementById('c6')
+const column7 = document.getElementById('c7')
+// Discs are styled under the classes "discRed" and "discBlack"
+// Columns have flex-direction: column-reverse
+
