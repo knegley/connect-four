f1cf27256a41d639722263464aaa5935b54f90ca
Kyle Negley <kpnegley@gmail.com>
2019-12-17 13:38:53 -0500

    got board to display chips and had to modify the css with the column mask to display chips

--- connectFour.js
+++ connectFour.js
@@ -1,12 +1,41 @@
 const columns = document.getElementsByClassName('column')
 // In case you need tags for individual columns. Feel free to remove these if you don't need them.
-const column1 = document.getElementById('c1')
-const column2 = document.getElementById('c2')
-const column3 = document.getElementById('c3')
-const column4 = document.getElementById('c4')
-const column5 = document.getElementById('c5')
-const column6 = document.getElementById('c6')
-const column7 = document.getElementById('c7')
-// Discs are styled under the classes "discRed" and "discBlack"
-// Columns have flex-direction: column-reverse
 
+
+let currentPlayer = 'red'
+let nextPlayer = 'black'
+
+const columnSelected = function() {
+    const column = event.currentTarget
+    if (column.childElementCount < 6) {
+        if (currentPlayer === 'red') {
+            let disc = document.createElement('div')
+            disc.className = 'discRed'
+            column.appendChild(disc)
+            nextPlayer = 'black'
+        } else {
+            let disc = document.createElement('div')
+            disc.className = 'discBlack'
+            column.appendChild(disc)
+            nextPlayer = 'red'
+        }
+        currentPlayer = nextPlayer
+    }
+};
+for (let i = 0; i < columns.length; i++) {
+    columns[i].addEventListener("click", columnSelected)
+}
+
+
+
+
+
+// Win condition
+
+function checkWin() {
+    const currentDisc = event.currentTarget.lastElementChild
+    let numberOfMatches = 0
+    const currentDiscPos = currentDisc.getBoundingClientRect()
+    
+    
+};
