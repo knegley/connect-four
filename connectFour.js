7ccfd7b776f5bf7d7a3213cbf7472a1210d02b29
Kyle Negley <kpnegley@gmail.com>
2019-12-18 22:52:59 -0500

    got win conditions to work

--- connectFour.js
+++ connectFour.js
@@ -1,7 +1,5 @@
 const columns = document.getElementsByClassName('column')
 const table = document.querySelector("#table");
-// In case you need tags for individual columns. Feel free to remove these if you don't need them.
-
 
 let currentPlayer = 'Red'
 let nextPlayer = 'Yellow'
@@ -34,10 +32,13 @@ for (let i = 0; i < columns.length; i++) {
     columns[i].addEventListener("click", columnSelected)
 };
 
+//function for the reset button
+function reset() {
+    window.location.reload()
+}
 
 
-
-// Testing Diagonal Win condition (not final)
+// Win Function
 function checkForWin(current) {
     let didWin = false
     let playerClass = 'discRed'
@@ -48,26 +49,13 @@ function checkForWin(current) {
     }
 
     // Diagonal
+
     for (let firstRow = 5; firstRow > 2; firstRow--) {
         let count = 0
-        for (let row = firstRow, col = 0; row  >= 0 && col < 6; row--, col ++) {
+        for (let row = firstRow, col = 0; row >= 0 && col < 6; row-- , col++) {
             let column = document.getElementById('table').children[col]
             if (column.children[row].className === playerClass) {
-                count ++
-            } else {
-                count = 0
-            }
-            if (count >= 4) {
-                didWin = true
-            }
-        }
-    }
-    for (let firstCol = 1; firstCol < 4; firstCol++) {
-        let count = 0
-        for (let col = firstCol, row = 5; col < 7 && row >= 0; row--, col ++) {
-            let column = document.getElementById('table').children[col]
-            if (column.children[row].className === playerClass) {
-                count ++
+                count++
             } else {
                 count = 0
             }
@@ -77,12 +65,12 @@ function checkForWin(current) {
         }
     }
 
-    for (let firstRow = 5; firstRow > 2; firstRow--) {
+    for (let firstCol = 1; firstCol < 4; firstCol++) {
         let count = 0
-        for (let row = firstRow, col = 6; row >2 && col > 0; row--, col --) {
+        for (let col = firstCol, row = 5; col < 7 && row >= 0; row-- , col++) {
             let column = document.getElementById('table').children[col]
             if (column.children[row].className === playerClass) {
-                count ++
+                count++
             } else {
                 count = 0
             }
@@ -91,12 +79,26 @@ function checkForWin(current) {
             }
         }
     }
-    for (let firstCol = 5; firstCol < 4; firstCol++) {
+    for (let firstRow = 5; firstRow > 2; firstRow--) {
         let count = 0
-        for (let col = firstCol, row = 5; col > 2 && row >= 0; row--, col --) {
+        for (let row = firstRow, col = 6; row >= 0 && col >= 0; row-- , col--) {
             let column = document.getElementById('table').children[col]
             if (column.children[row].className === playerClass) {
-                count ++
+                count++
+            } else {
+                count = 0
+            }
+            if (count >= 4) {
+                didWin = true
+            }
+        }
+    }
+    for (let firstCol = 5; firstCol > 2; firstCol--) {
+        let count = 0
+        for (let col = firstCol, row = 5; col >= 0 && row >= 0; row-- , col--) {
+            let column = document.getElementById('table').children[col]
+            if (column.children[row].className === playerClass) {
+                count++
             } else {
                 count = 0
             }
@@ -107,34 +109,34 @@ function checkForWin(current) {
     }
 
     // Horizontal
-    for (let row = 5; row >= 0; row --) {
+    for (let row = 5; row >= 0; row--) {
         let count = 0
-        for (let col = 0; col < 7; col ++) {
+        for (let col = 0; col < 7; col++) {
             let column = document.getElementById('table').children[col]
             if (column.children[row].className === playerClass) {
-                count ++
+                count++
             } else {
                 count = 0
             }
             if (count >= 4) {
                 didWin = true
-            }    
+            }
         }
     }
 
     // Vertical
-    for (let col = 0; col < 7; col ++) {
+    for (let col = 0; col < 7; col++) {
         let count = 0
-        for (let row = 5; row >= 0; row --) {
+        for (let row = 5; row >= 0; row--) {
             let column = document.getElementById('table').children[col]
             if (column.children[row].className === playerClass) {
-                count ++
+                count++
             } else {
                 count = 0
             }
             if (count >= 4) {
                 didWin = true
-            }    
+            }
         }
     }
     if (didWin) {
